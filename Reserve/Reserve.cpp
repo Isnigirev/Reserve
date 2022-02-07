24ecda17d171ec1bfae8c36b0252313b11f27074
IluaSN <ilua.snegirev.97@mail.ru>
2022-02-07 22:56:10 +1000

    Добавьте файлы проекта.

---
+++ Reserve/Reserve.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+
+//6-1//////////////////////////////////////////////////////////
+
+//void PrintArrMatrix(int iArr[], const int iSize)
+//{
+//    for (int i = 0; i < iSize; i++)
+//    {
+//        if (i % 3 == 0)
+//            std::cout << std::endl;
+//
+//        std::cout << iArr[i] << "\t";
+//    }
+//}
+//
+//void SortArr(int iArr[], const int iSize)
+//{
+//    int iSwapped = 0;
+//
+//    for (int i = 0; i < iSize - 1; i++)
+//    {
+//        if (iArr[i] > iArr[i+1])
+//        {
+//            int iSwapI = iArr[i + 1];
+//
+//            iArr[i + 1] = iArr[i];
+//
+//            iArr[i] = iSwapI;
+//
+//            iSwapped++;
+//        }
+//    }
+//    if (iSwapped > 0)
+//        SortArr(iArr, iSize);
+//    else
+//        return;
+//}
+
+//6-1//////////////////////////////////////////////////////////
+
+//6-3//////////////////////////////////////////////////////////
+
+void invertArrP (int iArrP[], const int iSizeP)
+{
+    for (int i = 0; i < iSizeP/2; i++)
+    {
+        int iSwapArrP = iArrP[i];
+
+        iArrP[i] = iArrP[iSizeP - 1- i];
+
+        iArrP[iSizeP - 1 - i] = iSwapArrP;
+    }
+
+    int i = 0;
+
+    do
+    {
+        i++;
+
+        if (sqrt(fabs(iArrP[i])) + 5 * pow(iArrP[i], 3) > 400)
+            std::cout << "Результат вычисления по формуле превысил 400 и составил-->" << sqrt(fabs(iArrP[i])) + 5 * pow(iArrP[i], 3) << std::endl;
+
+    } while (i < 11);
+
+}
+
+//6-3//////////////////////////////////////////////////////////
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+//6-1//////////////////////////////////////////////////////////
+    
+    //std::cout << "Было до сортировки:";
+
+    //const int iSize = 9;
+
+    //int iArr[iSize]{ 1, 9, 2, 5, 7, 6, 4, 3, 8 };
+
+    //PrintArrMatrix(iArr, iSize);
+
+    //SortArr(iArr, iSize);
+
+    //std::cout << std::endl << "Стало после сортировки:";
+
+    //PrintArrMatrix(iArr, iSize);
+    
+//6-1//////////////////////////////////////////////////////////
+
+//6-3//////////////////////////////////////////////////////////
+
+    const int iSizeP = 11;
+
+    int iArrP[iSizeP]{1,2,3,4,5,6,7,8,9,10,11};
+
+    int i = 0;
+
+    //do
+    //{
+    //    //std::cout << "Введите " << i + 1 << " число из 11" << std::endl;
+
+    //    //std::cin >> iArrP[i];
+
+    //    i++;
+
+    //} while (i < 11);
+
+    invertArrP(iArrP, iSizeP);
+
+//6-3//////////////////////////////////////////////////////////
+}
