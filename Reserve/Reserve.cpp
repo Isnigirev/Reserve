01fd317136d947864eae59630353ef985a7976ce
isnegirev34@gmail.com <isnegirev34@gmail.com>
2022-03-03 08:06:53 +1000

    start 10 1

--- Reserve/Reserve.cpp
+++ Reserve/Reserve.cpp
@@ -1,10 +1,32 @@
 ﻿#include <iostream>
-
+#define DEBUG
+//10.
 using namespace std;
+//1//
 
+void test(char cArr[])
+{
+    
+
+
+
+}
 int main()
 {
     setlocale(LC_ALL, "Ru");
+    //1//
+
+    
+    cout << "Ввежите скобочную послудовательность: "<< endl;
+    #ifdef DEBUG
+    char cArr[30]{"(())({})"};
+    #endif // DEBUG
+    {
+    char cArr[30]{};
+    cin >> cArr;
+    }
+    test(cArr);
+
 
     return 0;
 }
