abecec0279e86125e90a8270faeb7a86f4de5b98
isnegirev34@gmail.com <isnegirev34@gmail.com>
2022-03-04 07:58:07 +1000

    ..

--- Reserve/Reserve.cpp
+++ Reserve/Reserve.cpp
@@ -5,8 +5,24 @@ using namespace std;
 //1//
 
 void test(char cArr[])
-{
-    
+{   
+    int i = 0;
+
+    do
+    {
+        if (cArr[i] != ')' || cArr[i] != '}' || cArr[i] != ']')
+        {
+            break;
+        }
+        else 
+        {
+            cout << "Неверная скобочная последовательность" << endl;
+            return;
+        }
+
+        i++;
+
+    } while (i > 30);
 
 
 
@@ -16,16 +32,20 @@ int main()
     setlocale(LC_ALL, "Ru");
     //1//
 
-    
     cout << "Ввежите скобочную послудовательность: "<< endl;
-    #ifdef DEBUG
-    char cArr[30]{"(())({})"};
-    #endif // DEBUG
-    {
-    char cArr[30]{};
-    cin >> cArr;
-    }
-    test(cArr);
+    
+    #if DEBUG
+
+    #endif // 0
+
+        char cArr[30]{"(())({})"};
+        test(cArr);
+        char cArr[30]{};
+        cin >> cArr;
+        test(cArr);
+
+    //1//
+    
 
 
     return 0;
